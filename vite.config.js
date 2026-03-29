dc04a56a0431067b65bdc7ee8031e898f6c85ec9
Reggie <noreply@github.com>
2026-03-28 23:28:25 -0500

    v22 force new bundle filename

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name]-[hash]-v22.js`,
+        chunkFileNames: `assets/[name]-[hash]-v22.js`,
+      }
+    }
+  }
 })
-
