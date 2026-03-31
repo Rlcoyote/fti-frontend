5f64682e6b0d93e5fa388ccc66f26b819fa38e0d
Reggie <noreply@github.com>
2026-03-30 19:31:54 -0500

    v26.20 email signature + comments

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: `assets/[name]-[hash]-v26.19.js`,
-        chunkFileNames: `assets/[name]-[hash]-v26.19.js`,
-      }
-    }
-  }
-})
+  build: { rollupOptions: { output: { entryFileNames: "assets/app-26.20.js", chunkFileNames: "assets/chunk-26.20.js", assetFileNames: "assets/style-26.20[extname]" } } },
+  preview: { host: "0.0.0.0", port: process.env.PORT || 4173, allowedHosts: [".up.railway.app"] }
+});
